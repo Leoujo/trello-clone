3a1c8340570399c2cc8bd380b217770eaa69d17f
Leonardo Araujo <leoujooficial@gmail.com>
2022-05-04 19:37:33 -0300

    Third video

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import { Droppable } from "react-beautiful-dnd";
 import { Draggable } from "react-beautiful-dnd";
 
 const inicialItems = [
-  { id: "1", content: "Conteúdo 1" },
-  { id: "2", content: "Conteúdo 2" },
-  { id: "3", content: "Conteúdo 3" },
+  { id: "111", content: "Conteúdo 1" },
+  { id: "222", content: "Conteúdo 2" },
+  { id: "333", content: "Conteúdo 3" },
 ];
 
 const inicialColumns = [
@@ -17,25 +17,54 @@ const inicialColumns = [
     id: "123",
     items: inicialItems,
   },
+  {
+    name: "Doing",
+    id: "456",
+    items: [],
+  },
 ];
 
 function App() {
   const [columns, setColumns] = useState(inicialColumns);
 
+  const onDragEnd = (result) => {
+    var sourceColumnItems = columns[0].items;
+    var draggedItem = {};
+
+    for (var i in sourceColumnItems) {
+      if (sourceColumnItems[i].id == result.draggableId) {
+        draggedItem = sourceColumnItems[i];
+      }
+    }
+    // Excluí o objeto arrastado.
+    var filteredSourceColumnItems = sourceColumnItems.filter(
+      (item) => item.id != result.draggableId
+    );
+
+    // Adicionar o mesmo na nova posição.
+    filteredSourceColumnItems.splice(result.destination.index, 0, draggedItem);
+    console.log(filteredSourceColumnItems);
+
+    // Mudar o state
+    var columnsCopy = JSON.parse(JSON.stringify(columns));
+    columnsCopy[0].items = filteredSourceColumnItems;
+    setColumns(columnsCopy);
+  };
+
   return (
-    <div className="App">
-      <DragDropContext>
-        {inicialColumns.map((column) => (
-          <Droppable droppableId={column.id}>
-            {(provided) => (
-              <div
-                ref={provided.innerRef}
-                style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-              >
-                <h1>{column.name}</h1>
-                <div style={{ backgroundColor: "lightblue", width: 250, height: 500, padding: 10 }}>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <DragDropContext onDragEnd={onDragEnd}>
+        {columns.map((column) => (
+          <div style={{ display: "flex", flexDirection:"column", alignItems:"center" }}>
+            <h1>{column.name}</h1>
+            <Droppable droppableId={column.id} key={column.id}>
+              {(provided) => (
+                <div
+                  ref={provided.innerRef}
+                  style={{ backgroundColor: "lightblue", width: 250, height: 500, padding: 10, margin: 10 }}
+                >
                   {column.items.map((item, index) => (
-                    <Draggable draggableId={item.id} index={index}>
+                    <Draggable draggableId={item.id} index={index} key={item.id}>
                       {(provided) => (
                         <div
                           {...provided.dragHandleProps}
@@ -53,10 +82,11 @@ function App() {
                       )}
                     </Draggable>
                   ))}
+                  {provided.placeholder}
                 </div>
-              </div>
-            )}
-          </Droppable>
+              )}
+            </Droppable>
+          </div>
         ))}
       </DragDropContext>
     </div>
